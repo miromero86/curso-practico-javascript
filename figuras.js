43571144bc74bcbc7464a8c2bca0983bb144c647
Miguel Romero <miguelorvero8626@gmail.com>
2022-02-16 07:46:37 -0500

    Creé una corta estructura de HTML y se crearon las funciones en js para las figuras geometricas completo

---
+++ figuras.js
@@ -0,0 +1,73 @@
+// Codigo del Cuadrado
+
+console.group("Cuadrado");
+
+// const ladoCuadrado = 5;
+
+// console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cms");
+
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+// console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cms");
+
+function areaCuadrado(lado) {
+    return lado * lado;
+}
+// console.log("El area del cuadrado es: " + areaCuadrado + "cms2");
+
+console.groupEnd();
+
+// Codigo del Triangulo
+
+console.group("Triangulo");
+
+// const ladoTriangulo1 = 6;
+// const ladoTriangulo2 = 6;
+// const baseTriangulo = 4;
+// const alturaTriangulo = 5.5;
+
+// console.log("Los lados del triangulo miden: " + ladoTriangulo1 + "cms, " + ladoTriangulo2 + "cms, " + baseTriangulo + "cms.");
+
+// console.log("La altura del triangulo es: " + alturaTriangulo + "cms");
+
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+// console.log("El perimetro del triangulo es: " + perimetroTriangulo + "cms");
+
+function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+}
+// console.log("El area del triangulo es: " + areaTriangulo + "cms2");
+
+console.groupEnd();
+
+// Codigo del Circulo
+
+console.group("Circulo");
+
+// const radioCirculo = 4;
+// const diametroCirculo = radioCirculo * 2;
+const PI = Math.PI;
+
+// console.log("El radio del circulo es: " + radioCirculo + "cms, ");
+
+// console.log("El Diametro del circulo es: " + diametroCirculo + "cms, ");
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio)
+    return diametro * PI;
+}
+// console.log("El perimetro del circulo es: " + perimetroCirculo + "cms");
+
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+}
+// console.log("El area del circulo es: " + areaCirculo + "cms2");
+
+console.groupEnd();
