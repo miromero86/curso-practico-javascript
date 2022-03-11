3d83350a969a871542eeebe2fecafcbd18b6fdb5
Miguel Romero <miguelorvero8626@gmail.com>
2022-03-11 06:05:22 -0500

    Creé los documentos moda.html y moda.js, se crea la funcion para calcular la moda

---
+++ moda.js
@@ -0,0 +1,24 @@
+function calcularModa(lista){
+
+    const listaCount = {};
+    
+    lista.map(
+        function(elemento){
+            if(listaCount[elemento]){
+                listaCount[elemento] += 1;
+            } else{
+                listaCount[elemento] = 1;
+            }
+        }
+    );
+    
+    const listaArray = Object.entries(listaCount).sort(
+        function(primerValor, nuevoValor){
+            return primerValor[1] - nuevoValor[1];
+        }
+    );
+    
+    const moda = listaArray[listaArray.length -1];
+    
+    return moda
+    }
